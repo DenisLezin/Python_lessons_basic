38382d15f69d74a96fb111e6e6edd0b79d820089
DenisLezin <santar2000@mail.ru>
2019-03-29 00:13:04 +0300

    easy done

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,8 +5,10 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
-
+    frac_part = number % 1 * (10 ** ndigits)
+    if frac_part % 1 * 10 // 1 > 4:
+        frac_part +=1
+    return (number // 1) + (frac_part // 1 * (10 ** -ndigits))
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -20,7 +22,15 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    def sum_func(num):
+        return sum([int(i) for i in num])
+
+    if len(str(ticket_number)) != 6:
+        return 'You input wrong number'
+    elif sum_func(str(ticket_number)[:3]) == sum_func(str(ticket_number)[3:]):
+        return 'It is your lucky day! You WIN!'
+    else:
+        return 'You loose :('
 
 
 print(lucky_ticket(123006))
