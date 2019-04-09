871b62b776a5b30132a893cda812c8835a0c8a36
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-04-09 15:51:56 +0300

    Update hw05_normal.py
    успел только заготовки  для модуля сделать

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -10,6 +10,31 @@
 # и выводит результат действия: "Успешно создано/удалено/перешел",
 # "Невозможно создать/удалить/перейти"
 
+#-----------------------------------------------------------
+# заготовки для модуля
+# def make_dir1(directory):
+# #    directory = os.path.join(os.getcwd(), dname)
+#     if not os.path.exists(directory):
+#         os.mkdir(directory)
+#         print('Created directory: ' + dname)
+#     else:
+#         print('Directory alredy exists')
+#
+# def remove_dir1(directory):
+# #    directory = os.path.join(os.getcwd(), dname)
+#     if os.path.exists(directory):
+#         os.rmdir(directory)
+#         print('Deleted directory: ' + dname)
+#     else:
+#         print('There is no such directory')
+#------------------------------------------------------------
+from easy import make_dir1, remove_dir1
+import os
+import sys
+
+cur_dir = os.getcwd()
+
+
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
